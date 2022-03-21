3b18da5b9d9da9ee9370fd924e4fdf15c32eba26
soblab <soblab@Whit216-17.it.farmingdale.edu>
2022-03-21 09:53:29 -0400

    Added moveAll method

--- src/main/java/com/mycompany/shapeslabsa/PrimaryController.java
+++ src/main/java/com/mycompany/shapeslabsa/PrimaryController.java
@@ -2,6 +2,7 @@ package com.mycompany.shapeslabsa;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.transform.Translate;
 
@@ -16,11 +17,23 @@ public class PrimaryController {
     private Rectangle theRectangle;
 
     @FXML
+    private Circle theCircle;
+
+    @FXML
     private void moveRect() {
 
         Translate moveRect = new Translate();
         moveRect.setX(100);
         theRectangle.getTransforms().add(moveRect);
 
+    }
+
+    @FXML
+    private void moveAll() {
+        
+        Translate moveAll = new Translate();
+        moveAll.setX(200);
+        theRectangle.getTransforms().add(moveAll);
+        theCircle.getTransforms().add(moveAll);
     }
 }
