39cbf1ccda62f91d530d047259bfe61490e376fe
soblab <soblab@Whit216-17.it.farmingdale.edu>
2022-03-21 09:49:46 -0400

    Added move rectangle button

--- src/main/java/com/mycompany/shapeslabsa/PrimaryController.java
+++ src/main/java/com/mycompany/shapeslabsa/PrimaryController.java
@@ -2,11 +2,25 @@ package com.mycompany.shapeslabsa;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Translate;
 
 public class PrimaryController {
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
+    }
+
+    @FXML
+    private Rectangle theRectangle;
+
+    @FXML
+    private void moveRect() {
+
+        Translate moveRect = new Translate();
+        moveRect.setX(100);
+        theRectangle.getTransforms().add(moveRect);
+
     }
 }
